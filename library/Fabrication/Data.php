a641d6e4cfc0f8df6716b412d935c953031d809c
David Stevens <mail.davro@gmail.com>
2017-03-29 19:07:06 +0100

    Added Library Data

---
+++ library/Fabrication/Data.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Fabrication;
+
+/*
+ * This file is part of the fabrication framework.
+ * 
+ * David Stevens <mail.davro@gmail.com> <davro@davro.net>
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this version of the source code.
+ */
+
+/**
+ * Data Abstraction Layer.
+ *
+ * @package     Fabrication
+ * @subpackage  Data
+ * @author      David Stevens <mail.davro@gmail.com>
+ *
+ * @note Data
+ */
+class Data
+{
+    protected static $object;
+
+    /**
+     * Creation method.
+     *
+     * @return  object  Data
+     */
+    public static function create()
+    {
+        $calledClass = get_called_class();
+        self::$object = new $calledClass();
+        
+        return self::$object;
+    }
+}
